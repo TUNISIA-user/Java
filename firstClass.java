0f328984312ecd9cf95c8c1378f5a396f634cc46
Ghaith Nahdi <121437369+TUNISIA-user@users.noreply.github.com>
2025-06-05 02:07:03 +0100

    Create firstClass.java
    import java.util.HashMap;
    import java.util.ArrayList;
    
    
    
     class Person{
        int age;
        String name;
        boolean active;
        
        Person(int age,String name,boolean active){
          this.age = age;
          this.name  = name;
          this.active = active;
        } 
         
        } 
           
           
           
    public class Main {
     
      
       
       
        static <T> void print(T redux){
            System.out.println(redux);
            
             
        } 
        
      
      
      
      
      static void Fn(ArrayList <Person> objectr){
         
           for(Person loop: objectr){
             System.out.println(loop.name+" "+loop.age+" "+loop.active);
            
            } 
             
        } 
    
    
        static void go(ArrayList <Person> r ){
           System.out.println("person after sort");
            System.out.println(r);
           boolean  test = true;
           while (test) {
           test = false;
             for(int i =0;i<r.size()-1;i++){
             if(r.get(i).age >r.get(i+1).age) {
               Person aux = r.get(i);
               r.set(i,r.get(i+1));
               r.set(i+1,aux);
               test = true;
              
                 }
              
              }  
           
            }   
           
           System.out.println("person before sort");
           System.out.println(r);
         
         }
         
         
       static void fn( ArrayList <Person> ListOfPerson){
        
           
           for(Person index :ListOfPerson){
              
               System.out.println(index.name+" "+index.age+" "+index.active);
              } 
              
        }    
       
      
        public static void main(String[] args) {
    
            int[] array = {1, 5, 3, 4};
    
           // this coocked HashMap
            HashMap<Integer, Integer> store = new HashMap<>();
            ArrayList<Integer> Array = new ArrayList<>();
            ArrayList<Object> myList = new ArrayList<>();
            ArrayList<Person> ListOfPerson = new ArrayList<>();
    
            for (int Numberv : array) {
                Array.add(Numberv);
                if (store.containsKey(Numberv)) {
                    store.put(Numberv, store.get(Numberv) + 1);
                } else {
                    store.put(Numberv, 1);
                }
            }
    
            System.out.println("HashMap: " + store);
            System.out.println("Before sort: " + Array);
          
    
            boolean test = true;
               while (test) {
              test = false;
              for (int i = 0; i < Array.size() - 1; i++) {
                  if (Array.get(i) > Array.get(i + 1)) {
                      int aux = Array.get(i);
                      Array.set(i, Array.get(i + 1));
                      Array.set(i + 1, aux);
                      test = true;
                  }
              }
          }
    
            myList.add(10);             // Integer
            myList.add("Gaith");        // String
            myList.add(true);           // Boolean
            myList.add(3.14);           // Double
    
    
            System.out.println("After sort: " + Array);
            
            
            System.out.println("After myList: " + myList);
         
             // ADD ARRAY OF LIST
             ListOfPerson.add(new Person(43,"ghaith",true));
             ListOfPerson.add(new Person(10,"wajih",false));
             ListOfPerson.add(new Person(34,"hassen",true));
             fn(ListOfPerson); // this is name of the classes
             go(ListOfPerson);
             Fn(ListOfPerson);
             print(ListOfPerson);
            
        }
    }
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    import java.util.Arrays;
    
    static void say(Object value) {
        if (value instanceof Object[]) {
            System.out.println(Arrays.deepToString((Object[]) value));
        } else if (value instanceof int[]) {
            System.out.println(Arrays.toString((int[]) value));
        } else {
            System.out.println(value);
        }
    }

---
+++ firstClass.java
@@ -0,0 +1,171 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+
+
+
+ class Person{
+    int age;
+    String name;
+    boolean active;
+    
+    Person(int age,String name,boolean active){
+      this.age = age;
+      this.name  = name;
+      this.active = active;
+    } 
+     
+    } 
+       
+       
+       
+public class Main {
+ 
+  
+   
+   
+    static <T> void print(T redux){
+        System.out.println(redux);
+        
+         
+    } 
+    
+  
+  
+  
+  
+  static void Fn(ArrayList <Person> objectr){
+     
+       for(Person loop: objectr){
+         System.out.println(loop.name+" "+loop.age+" "+loop.active);
+        
+        } 
+         
+    } 
+
+
+    static void go(ArrayList <Person> r ){
+       System.out.println("person after sort");
+        System.out.println(r);
+       boolean  test = true;
+       while (test) {
+       test = false;
+         for(int i =0;i<r.size()-1;i++){
+         if(r.get(i).age >r.get(i+1).age) {
+           Person aux = r.get(i);
+           r.set(i,r.get(i+1));
+           r.set(i+1,aux);
+           test = true;
+          
+             }
+          
+          }  
+       
+        }   
+       
+       System.out.println("person before sort");
+       System.out.println(r);
+     
+     }
+     
+     
+   static void fn( ArrayList <Person> ListOfPerson){
+    
+       
+       for(Person index :ListOfPerson){
+          
+           System.out.println(index.name+" "+index.age+" "+index.active);
+          } 
+          
+    }    
+   
+  
+    public static void main(String[] args) {
+
+        int[] array = {1, 5, 3, 4};
+
+       // this coocked HashMap
+        HashMap<Integer, Integer> store = new HashMap<>();
+        ArrayList<Integer> Array = new ArrayList<>();
+        ArrayList<Object> myList = new ArrayList<>();
+        ArrayList<Person> ListOfPerson = new ArrayList<>();
+
+        for (int Numberv : array) {
+            Array.add(Numberv);
+            if (store.containsKey(Numberv)) {
+                store.put(Numberv, store.get(Numberv) + 1);
+            } else {
+                store.put(Numberv, 1);
+            }
+        }
+
+        System.out.println("HashMap: " + store);
+        System.out.println("Before sort: " + Array);
+      
+
+        boolean test = true;
+           while (test) {
+          test = false;
+          for (int i = 0; i < Array.size() - 1; i++) {
+              if (Array.get(i) > Array.get(i + 1)) {
+                  int aux = Array.get(i);
+                  Array.set(i, Array.get(i + 1));
+                  Array.set(i + 1, aux);
+                  test = true;
+              }
+          }
+      }
+
+        myList.add(10);             // Integer
+        myList.add("Gaith");        // String
+        myList.add(true);           // Boolean
+        myList.add(3.14);           // Double
+
+
+        System.out.println("After sort: " + Array);
+        
+        
+        System.out.println("After myList: " + myList);
+     
+         // ADD ARRAY OF LIST
+         ListOfPerson.add(new Person(43,"ghaith",true));
+         ListOfPerson.add(new Person(10,"wajih",false));
+         ListOfPerson.add(new Person(34,"hassen",true));
+         fn(ListOfPerson); // this is name of the classes
+         go(ListOfPerson);
+         Fn(ListOfPerson);
+         print(ListOfPerson);
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import java.util.Arrays;
+
+static void say(Object value) {
+    if (value instanceof Object[]) {
+        System.out.println(Arrays.deepToString((Object[]) value));
+    } else if (value instanceof int[]) {
+        System.out.println(Arrays.toString((int[]) value));
+    } else {
+        System.out.println(value);
+    }
+}
+
