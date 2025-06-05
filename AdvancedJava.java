f2ae0ec6b515ed19555525ca76949ca511cf4b00
Ghaith Nahdi <121437369+TUNISIA-user@users.noreply.github.com>
2025-06-05 17:06:40 +0100

    Update AdvancedJava.java
    public class Student {
        static String schoolName = "Tunis High School"; // متغير ثابت
    
        String studentName;
    
        public Student(String studentName) {
            this.studentName = studentName;
        }
    
        void printInfo() {
            System.out.println("Name: " + studentName);
            System.out.println("School: " + schoolName); // نستخدم مباشرة static variable
        }
    
        public static void main(String[] args) {
            Student s1 = new Student("Ahmed");
            Student s2 = new Student("Fatma");
    
            s1.printInfo();
            s2.printInfo();
    
            // يمكن الوصول مباشرة لمتغير static بدون كائن
            System.out.println(Student.schoolName);
        }
    }

--- AdvancedJava.java
+++ AdvancedJava.java
@@ -18,3 +18,30 @@
 
 
     // @Override this methode make the real  value not the adrees
+// statict type for java this mean exmple all people studnet in school let say harvad 
+// ok  and in this school have Type <Person > read in this school so  each <Person> diffrent in auhtor <Person> LIKE <name,age,smart,...>
+public class Student {
+    static String schoolName = "Tunis High School"; // متغير ثابت
+
+    String studentName;
+
+    public Student(String studentName) {
+        this.studentName = studentName;
+    }
+
+    void printInfo() {
+        System.out.println("Name: " + studentName);
+        System.out.println("School: " + schoolName); // نستخدم مباشرة static variable
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Ahmed");
+        Student s2 = new Student("Fatma");
+
+        s1.printInfo();
+        s2.printInfo();
+
+        // يمكن الوصول مباشرة لمتغير static بدون كائن
+        System.out.println(Student.schoolName);
+    }
+}
