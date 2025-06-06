c5b1713424a6a381fd734564e445b874155254ce
SAFIR jHanm🔥 <121437369+TUNISIA-user@users.noreply.github.com>
2025-06-06 19:19:08 +0100

    Update and rename switch to switch.java
    ||  in java

--- switch.java
+++ switch.java
@@ -16,3 +16,8 @@ public class Grook {
         
     }
 }
+
+// here if you wana do in  one case like if the value of n  1  or  2  or 3  you  can do this 
+// in case you can de case 1,2,3,4 ->  System.out.println(your display here)
+
+ 
