2077d4334a566985e6ec22f09cc11d2bcc45c79c
SAFIR jHanm🔥 <121437369+TUNISIA-user@users.noreply.github.com>
2025-06-20 23:04:07 +0100

    Update StructueData.java

--- StructueData.java
+++ StructueData.java
@@ -1,5 +1,5 @@
  import java.util.concurrent.ConcurrentHashMap;
-  
+   
 public class ConcurrentHashMapExample {
     public static void main(String[] args) {
         ConcurrentHashMap<String, Integer> map = new ConcurrentHashMap<>();
